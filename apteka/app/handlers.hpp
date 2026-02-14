4b4c9b54e4f9c0b91358da7a59692dc59c165dd0
VadikPtr <va00iosd@gmail.com>
2026-02-15 02:01:28 +0500

    filter photos

--- apteka/app/handlers.hpp
+++ apteka/app/handlers.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "app.hpp"
 #include "http-server/content-type.hpp"
+#include <cc/sarr.hpp>
 
 class ExampleHandler : public IReqHandler {
   AppContext& app_context;
@@ -12,13 +13,17 @@ class ExampleHandler : public IReqHandler {
     TemplateInstance& photo_tmpl = app_context.template_engine.get("photo"_sh);
     TemplateInstance& index_tmpl = app_context.template_engine.get("index"_sh);
 
-    Str photos = photo_tmpl.render_array(                   //
-        app_context.db.get_photos().get_all().sub(10, 30),  //
-        [](const Photo& photo, TemplateKV& kv) {
-          kv.insert("id"_sh, photo.id)
-              .insert("source_name"_sh, photo.source_name)
-              .insert("date_created"_sh, photo.date_created)
-              .insert("category"_sh, photo.category->name);
+    Arr<Photo>&     db_photos = app_context.db.get_photos().get_all();
+    PhotoFilter<30> photos_filtered;
+    photos_filtered.filter(app_context.db.get_photos().get_all(),
+                           [](const Photo& photo) { return photo.category->name != "hidden"; });
+
+    Str photos =
+        photo_tmpl.render_array(photos_filtered.data, [](const Photo* photo, TemplateKV& kv) {
+          kv.insert("id"_sh, photo->id)
+              .insert("source_name"_sh, photo->source_name)
+              .insert("date_created"_sh, photo->date_created)
+              .insert("category"_sh, photo->category->name);
         });
 
     Str body = index_tmpl.render(  //
@@ -31,6 +36,25 @@ class ExampleHandler : public IReqHandler {
         .body(move(body))
         .send();
   }
+
+  template <size_t capacity>
+  struct PhotoFilter {
+    Photo*       data_arr[capacity] = {nullptr};
+    SArr<Photo*> data               = SArr<Photo*>(data_arr);
+
+    template <typename TFunc>
+    void filter(Arr<Photo>& source, TFunc&& func) {
+      for (size_t i = 0; i < source.size(); i++) {
+        Photo& photo = source[i];
+        if (func(photo)) {
+          data.push(&photo);
+          if (data.size() == data.capacity()) {
+            break;
+          }
+        }
+      }
+    }
+  };
 };
 
 class RedirectHandler : public IReqHandler {
